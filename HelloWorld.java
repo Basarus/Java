ed464fbeab0bfb0ba6fd4309c6502659cb4f1219
Basar <74671944+Basarus@users.noreply.github.com>
2021-10-11 22:32:17 +0300

    upd

--- HelloWorld.java
+++ HelloWorld.java
@@ -36,6 +36,19 @@ public class HelloWorld {
         return value;
     }
 
+    public static boolean isPowerOfTwo(int value) {
+        value = Math.abs(value);
+        String bin_str = Integer.toBinaryString(value);
+        long k = 0;
+        for (int i = 0; i < bin_str.length(); i++)
+            if (bin_str.charAt(i) == '1')
+                k++;
+        if (k == 1)
+            return true;
+        else
+            return false;
+    }
+
 }
 
 // jar cfe.hw.jar HelloWorld HelloWorld.class для сборки класса
